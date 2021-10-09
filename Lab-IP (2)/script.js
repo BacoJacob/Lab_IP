ddd069cd1f65724d08c95285cc991ea4158cd99e
BacoJacob <63606163+BacoJacob@users.noreply.github.com>
2021-10-08 20:37:58 -0400

    Lab-6
    Added lab 6

---
+++ Lab-IP (2)/script.js
@@ -0,0 +1,16 @@
+function lastUpdated() {
+  var x = document.lastModified;
+  document.getElementById("date").innerHTML = x;
+}
+
+function backgroundGreen() {
+  document.getElementById("change").style.backgroundColor = "green";
+}
+
+function backgroundPurple() {
+  document.getElementById("change").style.backgroundColor = "purple";
+}
+
+function backgroundRed() {
+  document.getElementById("change").style.backgroundColor = "red";
+}
